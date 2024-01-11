82632e093beb7c15cb6d1b67935d2b7caa056979
Victor Candido <victorev@outlook.com>
2024-01-11 17:39:50 -0300

    Começo da implementação das rotas para criação de apontamentos

---
+++ app/api/appointment/route.ts
@@ -0,0 +1,20 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+// export async function GET(req: Request) {
+//     try {
+//         const appointments = await db.appointment.findMany();
+//         return NextResponse.json(appointments);
+//     } catch (error) {
+//         console.log('[ERROR] GET Appointment - ', error);
+//         return new NextResponse('Internal Error', { status: 500 });
+//     }
+// }
+
+// export async function POST(req: Request) {
+//     try {
+//         const 
+//     } catch (error) {
+        
+//     }
+// }
