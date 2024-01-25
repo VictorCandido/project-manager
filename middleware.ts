aca07dec277c43dd00b8205951976cbd56101931
Victor Candido <victorev@outlook.com>
2024-01-25 15:33:04 -0300

    Delete appointment-related API routes and update middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,6 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-      // publicRoutes: ['/api/customer', '/api/customer/[customerId]', '/api/appointment'],
-      // apiRoutes: ['/customer']
-
       apiRoutes: ['/(api|trpc)(.*)'],
       afterAuth(auth, req, evt) {
             if (auth.isApiRoute || auth.isPublicRoute) {
