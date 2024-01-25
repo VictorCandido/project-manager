b08d0f71dc5acb8e9b65f3b86a008cc4264c8f3b
Victor Candido <victorev@outlook.com>
2024-01-25 16:56:54 -0300

    Returning AppointmentDaySkeleton

---
+++ components/Appointment/AppointmentDaySkeleton.tsx
@@ -0,0 +1,17 @@
+
+import { Skeleton } from "../ui/skeleton";
+import AppointmentCardSkeleton from "./AppointmentCardSkeleton";
+
+const AppointmentDaySkeleton = () => {
+    return (
+        <div className="flex flex-col gap-4">
+            <Skeleton className="h-4 w-64" />
+
+            <AppointmentCardSkeleton />
+            <AppointmentCardSkeleton />
+            <AppointmentCardSkeleton />
+        </div>
+    );
+}
+
+export default AppointmentDaySkeleton;
